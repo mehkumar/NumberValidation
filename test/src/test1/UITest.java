bee8b5657be893ae68e8303c53a1fce990c18e46
mehkumar <49111550+mehkumar@users.noreply.github.com>
2019-10-13 18:30:59 +0530

    Final code

--- test/src/test1/UITest.java
+++ test/src/test1/UITest.java
@@ -1,18 +1,26 @@
 package test1;
 
+
 import java.util.Scanner;
 
 public class UITest {
 	
 	public static void main(String[] args) {
-		
+		int number=0;
 		Scanner s1 =new Scanner(System.in);
-		System.out.println("Enter the number");
-	    int number=s1.nextInt();
-		ValidateInputImpl v=new ValidateInputImpl ();
-		v.validate(number);
-		s1.close();
-		
-	}
-
-}
+		System.out.println("Enter a number");
+		try {
+			  number=s1.nextInt();
+			  ValidateInputImpl v=new ValidateInputImpl ();
+			  v.validate(number);
+		}
+		catch(Exception e)
+		{
+			System.out.println("Enter a positive numerical value");
+			
+		}
+		finally
+		{
+				s1.close();	
+			
+		}
