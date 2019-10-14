e1a9b89e9227b1db97459695e1b96504d2660bec
mehkumar <49111550+mehkumar@users.noreply.github.com>
2019-10-14 14:31:10 +0530

    Update UITest.java

--- test/src/test1/UITest.java
+++ test/src/test1/UITest.java
@@ -1,19 +1,24 @@
 package test1;
 import java.util.Scanner;
 public class UITest {
-	
-		public static void main(String[] args) {
+
+		//Main Function
+		public static void main(String[] args){
+		
+		//Initializing input variable			
 		int number=0;
 		Scanner s1 =new Scanner(System.in);
 		System.out.println("Enter a number");
 		try {
 			  number=s1.nextInt();
 			  ValidateInputImpl v=new ValidateInputImpl ();
+			  
+			  //Function call for invoking the validator function
 			  v.validate(number);
 		}
 		catch(Exception e)
 		{
-			System.out.println("Enter a positive numerical value");
+			System.out.println("Enter a non-decimal numerical value");
 			
 		}
 		finally
